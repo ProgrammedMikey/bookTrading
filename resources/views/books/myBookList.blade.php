06d38933e0cd4aa5172e6062a56c3b4c06541d57
Michael DaSilva <michaeldasilva@Michaels-MacBook-Pro.local>
2017-01-14 11:36:01 -0500

    booklist css

--- resources/views/books/myBookList.blade.php
+++ resources/views/books/myBookList.blade.php
@@ -1,6 +1,6 @@
 <br>
 @foreach($mybooks as $book)
-<div class="card col-md-3">
+<div class="card col-md-3 backColor depth">
     <img src={{ $book->body }} alt="Image" height="230" width="230" style="width:100%">
     <div class="container cardContainer">
         <p>{{ $book->title }}</p>
